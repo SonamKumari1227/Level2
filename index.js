e917130ded871cff73ac55ed9cba5f0f661b5a54
sonam <er.sonam2712kumari@gmail.com>
2024-03-03 20:35:01 +0530

    deploying files

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express")
+const ejs = require('ejs');
+const path = require("path");
+
 const app = express();
-app.get('/', (req, res) => {
-    res.send("hello , am i visible to you,,,,,,,,,,,,,,")
-})
 
+// Set up environment variables for MongoDB connection
+app.set('views', path.join(__dirname, 'views'));
+console.log("path= ", path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
+app.get('/', async (req, res) => {
+    try {
+      
+        res.render('index');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 app.listen(8050, () => {
     console.log("server is on..........")
 })
